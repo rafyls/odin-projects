05bb22b203ebdc280316d3e6f4429914101d7474
rafyls <181994883+rafyls@users.noreply.github.com>
2025-07-14 10:56:43 +0100

    Add the computer choice logic

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let num = Math.random();
+  num = num * 30;
+  if ((num >= 0) && (num < 10)) {
+    return "rock";
+  } else if (((num >= 10) && (num < 20))) {
+    return "paper";
+  } else {
+    // num in this case is greater than or equal to 20 and less than 30
+    return "scissors";
+  }
+}
