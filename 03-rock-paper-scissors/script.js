133aff92f9d129ca00c7da98309872583a7bbec0
rafyls <181994883+rafyls@users.noreply.github.com>
2025-07-20 15:16:08 +0100

    Modify the game logic to support the new UI

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -19,75 +19,151 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+let humanSelection = "";
+let computerSelection = "";
+let numberRounds = 1;
+
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
 
+  let container = document.querySelector("#container-result");
+
+  let roundResult = document.createElement("p");
+  roundResult.style.fontWeight = "bold";
+
   if (humanChoice === "rock") {
     if (computerChoice === "scissors") {
-      console.log("You win! Rock beats scissors.");
+      roundResult.textContent = "You win this round! Rock beats scissors."
       humanScore += 1;
     } else if (computerChoice === "paper") {
-      console.log("You lose! Paper beats rock.");
+      roundResult.textContent = "You lose this round! Paper beats rock.";
       computerScore += 1;
     } else {
-      console.log("It's a tie, try again!");
+      roundResult.textContent = "It's a tie, try again!";
     }
   }
 
   if (humanChoice === "paper") {
     if (computerChoice === "rock") {
-      console.log("You win! Paper beats rock.");
+      roundResult.textContent = "You win this round! Paper beats rock.";
       humanScore += 1;
     } else if (computerChoice === "scissors") {
-      console.log("You lose! Scissors beat paper.");
+      roundResult.textContent = "You lose this round! Scissors beat paper.";
       computerScore += 1;
     } else {
-      console.log("It's a tie, try again!");
+      roundResult.textContent = "It's a tie, try again!";
     }
   }
 
   if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
-      console.log("You win! Scissors beat paper.");
+      roundResult.textContent = "You win this round! Scissors beat paper.";
       humanScore += 1;
     } else if (computerChoice === "rock") {
-      console.log("You lose! Rock beats scissors.");
+      roundResult.textContent = "You lose this round! Rock beats scissors.";
       computerScore += 1;
     } else {
-      console.log("It's a tie, try again!");
+      roundResult.textContent = "It's a tie, try again!";
     }
   }
+
+  container.appendChild(roundResult);
+
 }
 
-function playGame() {
-  for (let i = 1; i < 6; i++) {
-    console.log("ROUND " + i);
+function printRoundInfo(humanChoice, computerChoice, roundNumber) {
+  let container = document.querySelector("#container-result");
+  
+  let roundNum = document.createElement("h2");
+  roundNum.textContent = "ROUND " + roundNumber;
+  container.appendChild(roundNum);
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  let humanPick = document.createElement("p");
+  humanPick.textContent = "Your choice for this round is: " + humanChoice;
+  container.appendChild(humanPick);
 
-    console.log("Your choice for this round is: " + humanSelection);
+  let computerPick = document.createElement("p");
+  computerPick.textContent = "The computer choice is: " + computerChoice;
+  container.appendChild(computerPick);
+}
 
-    console.log("The computer choice is: " + computerSelection);
+function printRunningScore() {
+  let container = document.querySelector("#container-result");
+  
+  let humanScoreElement = document.createElement("p");
+  humanScoreElement.textContent = "Your current score: " + humanScore;
+  container.appendChild(humanScoreElement);
 
-    playRound(humanSelection, computerSelection);
-  }
+  let computerScoreElement = document.createElement("p");
+  computerScoreElement.textContent = "Current computer score: " + computerScore;
+  container.appendChild(computerScoreElement);
+}
 
-  if (computerScore < humanScore) {
-    return "human";
-  } else if (humanScore < computerScore) {
-    return "computer";
+function displayWinnerInfo(result) {
+  let container = document.querySelector("#container-result");
+
+  let finalScore = document.createElement("h2");
+  finalScore.textContent = "Final Score";
+  container.appendChild(finalScore);
+
+  let finalResult = document.createElement("p");
+
+  if (result === "human") {
+    finalResult.textContent = "You won the game. Final score: " + humanScore;
+  } else if (result === "computer") {
+    finalResult.textContent = "Computer won the game. Final score: " + computerScore
   } else {
-    return "none";
+    finalResult.textContent = "It's a tie score. Final score: " + humanScore
   }
+
+   container.appendChild(finalResult);
 }
 
-result = playGame();
+function playGame(humanSelection, computerSelection, numberRounds) {
+  printRoundInfo(humanSelection, computerSelection, numberRounds);
 
-if (result === "human") {
-  console.log("You won the game. Final score: " + humanScore);
-} else if (result === "computer") {
-  console.log("Computer won the game. Final score: " + computerScore);
-} else {
-  console.log("It's a tie score. Final score: " + humanScore);
+  playRound(humanSelection, computerSelection);
+
+  printRunningScore();
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (numberRounds === 6) {
+      let container = document.querySelector("#container-result");
+
+      let message = document.createElement("h2");
+      message.textContent = "Maximum number of rounds reached, reload the page to play again.";
+      container.appendChild(message);
+    } else {
+      switch (button.id) {
+        case 'rock':
+          computerSelection = getComputerChoice();
+          playGame('rock', computerSelection, numberRounds);
+          numberRounds++;
+          break;
+        case 'paper':
+          computerSelection = getComputerChoice();
+          playGame('paper', computerSelection, numberRounds);
+          numberRounds++;
+          break;
+        case 'scissors':
+          computerSelection = getComputerChoice();
+          playGame('scissors', computerSelection, numberRounds);
+          numberRounds++;
+          break;
+      }
+
+      if (numberRounds === 6) {
+        if (computerScore < humanScore) {
+          displayWinnerInfo("human");
+        } else if (humanScore < computerScore) {
+          displayWinnerInfo("computer");
+        } else {
+          displayWinnerInfo("none");
+        }
+      }
+    }
+  })
+});
