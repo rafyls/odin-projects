456bbeff5eaebef7fcf7b08490cfe47f31f1fdca
rafyls <181994883+rafyls@users.noreply.github.com>
2025-07-21 17:32:44 +0100

    Set the dimensions of each grid item dynamically
    The width and height of each grid item (div) were
    set with fixed values. Besides, the grid container
    didn't have a defined width and height.

--- 04-etch-a-sketch/script.js
+++ 04-etch-a-sketch/script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 256; i++) {
+const INITIAL_NUM_SQUARES = 16;
+const INITIAL_GRID_SIZE = 256
+
+for (let i = 1; i <= INITIAL_GRID_SIZE; i++) {
   const divItem = document.createElement("div");
   divItem.classList.add("div-item");
   
@@ -14,6 +17,12 @@ for (let i = 1; i <= 256; i++) {
 
     container.appendChild(divItemInvisible);
   }
+
+  const widthContainer = (container.offsetWidth) / INITIAL_NUM_SQUARES;
+  const heightContainer = (container.offsetHeight) / INITIAL_NUM_SQUARES;
+
+  divItem.style.width = widthContainer.toString() + "px";
+  divItem.style.height = heightContainer.toString() + "px";
 
   container.appendChild(divItem);
 }
