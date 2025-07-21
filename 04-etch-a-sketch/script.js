288f3fc32ebd8c2c47b63b2689f51123409857a9
rafyls <181994883+rafyls@users.noreply.github.com>
2025-07-21 11:50:08 +0100

    Create a webpage with a 16 x 16 grid of squares

---
+++ 04-etch-a-sketch/script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 256; i++) {
+  const divItem = document.createElement("div");
+  divItem.classList.add("div-item");
+  
+  if (((i % 16) === 1) && (i != 1)) {
+    const divItemInvisible = document.createElement("div");
+
+    divItem.classList.add("div-item-invisible");
+
+    divItemInvisible.style.width = "100%";
+    divItemInvisible.style.height = "0";
+
+    container.appendChild(divItemInvisible);
+  }
+
+  container.appendChild(divItem);
+}
