3012b6e151c05acc4dcf0ad37767a4abf2d3ba00
rafyls <181994883+rafyls@users.noreply.github.com>
2025-07-26 12:01:07 +0100

    Add click event listeners of the digit and decimal separator buttons

--- 05-calculator/script.js
+++ 05-calculator/script.js
@@ -9,14 +9,61 @@ const operatorButtons = document.querySelectorAll(".operator-btn");
 const equalButton = document.querySelector(".equal-btn");
 const clearButton = document.querySelector(".clear-btn");
 
+const display = document.querySelector(".display");
+
+const displayParagraph = document.querySelector(".display p");
+
+let firstDigitClick = true;
+let firstDecimalSepClick = true;
+
+function numberOfDecimalSep() {
+  const number = displayParagraph.textContent.split("");
+
+  let numDecimalSep = 0;
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] === ".") {
+      numDecimalSep++;
+    }
+  }
+
+  return numDecimalSep;
+}
+
 digitButtons.forEach((digitButton) => {
   digitButton.addEventListener("click", () => {
-    alert(digitButton.textContent);
+
+    if (firstDigitClick) {
+      firstDigitClick = false;
+
+      displayParagraph.textContent = "";
+      
+      displayParagraph.textContent = displayParagraph.textContent + digitButton.textContent;
+    } else {
+      displayParagraph.textContent = displayParagraph.textContent + digitButton.textContent;
+    }
+
+    if ((operand1 === null) || (lastOperation === null)) {
+      operand1 = Number.parseFloat(displayParagraph.textContent);
+    } else {
+      operand2 = Number.parseFloat(displayParagraph.textContent);
+    }
   });
 });
 
 decimalSepButton.addEventListener("click", () => {
-    alert(decimalSepButton.textContent);
+  if (firstDecimalSepClick) {
+    firstDecimalSepClick = false;
+    displayParagraph.textContent = displayParagraph.textContent + decimalSepButton.textContent;
+  } else {
+    let numDecimalSep = numberOfDecimalSep();
+
+    if (numDecimalSep === 0) {
+      displayParagraph.textContent = displayParagraph.textContent + decimalSepButton.textContent;
+      decimalSepButton.disabled = false;
+    } else {
+      decimalSepButton.disabled = true;
+    }
+  }
 });
 
 
