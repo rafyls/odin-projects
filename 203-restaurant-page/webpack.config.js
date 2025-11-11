3d19c137981e9146af8fa73a453b712c54bf8240
rafyls <181994883+rafyls@users.noreply.github.com>
2025-11-11 16:20:47 +0100

    Update Webpack config to handle processed images

--- 203-restaurant-page/webpack.config.js
+++ 203-restaurant-page/webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+         generator: {
+          filename: 'assets/images/[name][hash][ext][query]',
+        },
       },
     ],
   },
